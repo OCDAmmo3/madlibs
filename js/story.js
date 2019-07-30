c6ee1a2bb004bf8926a59cad371de603fcd7bd8e
Zscoob <james.zobian@gmail.com>
2019-07-30 14:17:02 -0500

    pulled logs

--- js/story.js
+++ js/story.js
@@ -1,30 +1,26 @@
+'use strict';
 /*TODO
 Build random selector function
-
-
 Constructor function for var paragraph to have an easier time
 building story with swapping parts.
-
 get elements from form
-
 push elements to story
-
 Build CreateParagraph function
 */
-'use strict'
-function Paragraph(person,adjective,place,thing) {
-    this.person= '',
-    this.adjective= '',
-    this.place= '',
-    this.thing= '',
-    randomSelector = function(array){
-    var random = Math.floor(Math.random * array.length);
+
+var Paragraph = function(person,adjective,place,thing) {
+    this.person = '';
+    this.adjective = '';
+    this.place = '';
+    this.thing = '';
+    this.randomSelector = function(array){
+        var random = Math.floor(Math.random * array.length);
 };
 }
 
-function giveRandom(array){
-    var random = array(randomSelector(array));
-    return random
+function giveRandom(array) {
+  var random = array(randomSelector(array));
+  return random;
 };
 
 var selectPerson = document.getElementById('person').value;
@@ -34,10 +30,10 @@ var selectThing = document.getElementById('thing').value;
 
 var userInput = new Paragraph(selectPerson, selectAdjective, selectPlace, selectThing);
 
-function publishStory(){
-    var paragraph = createParagraph();
-    var story = document.getElementById('story');
-    var p = document.createElementById('p');
-    p.textContent= paragraph;
-    story.appendChild(p);
+function publishStory() {
+  var paragraph = createParagraph();
+  var story = document.getElementById('story');
+  var p = document.createElementById('p');
+  p.textContent = paragraph;
+  story.appendChild(p);
 }
