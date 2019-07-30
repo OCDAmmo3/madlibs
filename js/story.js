b364920912f0ad4cccbb379acbe0a929a697d785
Froconic <froconicdesign@gmail.com>
2019-07-30 14:19:58 -0500

    added the createParagraph function and necessary compnenents

--- js/story.js
+++ js/story.js
@@ -18,22 +18,34 @@ var Paragraph = function(person,adjective,place,thing) {
     this.adjective = '';
     this.place = '';
     this.thing = '';
-    this.randomSelector = function(array){
+    this.randomSelector = function (array) {
         var random = Math.floor(Math.random * array.length);
+      };
 };
-}
+
+//=============================================================================
 
 function giveRandom(array) {
   var random = array(randomSelector(array));
   return random;
 };
+//=============================================================================
 
-var selectPerson = document.getElementById('person').value;
-var selectAdjective = document.getElementById('adjective').value;
-var selectPlace = document.getElementById('place').value;
-var selectThing = document.getElementById('thing').value;
+//CREATE PARAGRAPH FUNCTION=============================================================================
+function createParagraph() {
 
-var userInput = new Paragraph(selectPerson, selectAdjective, selectPlace, selectThing);
+  var people = JSON.Parse(localStorage.getItem('person'));
+  var places = JSON.Parse(localStorage.getItem('place'));
+  var adjectives = JSON.Parse(localStorage.getItem('adjective'));
+  var things = JSON.Parse(localStorage.getItem('thing'));
+  var newStory = new Paragraph(people, places, adjectives, things);
+  var story = document.getElementById('createStory');
+
+  story.addEventListener('click', HandleSubmitStory);
+}
+
+//=============================================================================
+
 
 function publishStory() {
   var paragraph = createParagraph();
