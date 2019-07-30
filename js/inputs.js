5d764d01be2f3f30b2263ab46604dd639d2b8452
Terra Huber <swiftruns26@gmail.com>
2019-07-30 11:36:54 -0500

    Added handleSubmitStory function

--- js/inputs.js
+++ js/inputs.js
@@ -64,10 +64,13 @@ function saveToLocalStorage() {
 //==============================================================================
 
 //handleSubmitStory Function====================================================
+function handleSubmitStory(event){
 //TODO:Prevent default action
-
+event.preventDefaul();
 //TODO:Call the functions createParagraph() and publishStory()
-
+createParagraph();
+publishStory();
+}
 
 //==============================================================================
 
